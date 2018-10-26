a927a1d67e3531e86509e4271832ec6f5c3e5d65
tthiagoramalhot <38708640+tthiagoramalhot@users.noreply.github.com>
2018-10-26 14:13:45 -0300

    nova main

--- main.py
+++ main.py
@@ -1,46 +1,7 @@
-alfabeto = ['a', 'b', 'c', 'd', 'e', 'f',
-            'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r',
-            's', 't', 'u', 'v', 'w', 'x',
-            'y', 'z'
-]
+import funcoes
 
-listaCaracter = []
-novoNome = []
+opcao = input('Criptografar(CP) ou Descriptografar(DP) : ')
+texto = input('Digite um tetxto : ')
 
-nome = input('Digite um texto para criptografar: ')
-
-# Desmenbrando texto
-for contador in range(1, len(nome) + 1):
-  letra = nome[contador - 1]
-  listaCaracter.append(letra)
-
-# Criptografando texto
-for i in range(len(listaCaracter)):
-  for j in range(len(alfabeto)):
-    if listaCaracter[i] == alfabeto[j]:
-      novoCaracter = alfabeto[j + 3]
-      novoNome.append(novoCaracter)
-
-m = ''.join(novoNome)
-print('Texto criptografado:', m)
-
-del listaCaracter[:]
-del novoNome[:]
-
-nome = input('Digite um texto para descriptografar: ')
-
-# Desmenbrando texto
-for contador in range(1, len(nome) + 1):
-  letra = nome[contador - 1]
-  listaCaracter.append(letra)
-
-# Desriptografando texto
-for i in range(len(listaCaracter)):
-  for j in range(len(alfabeto)):
-    if listaCaracter[i] == alfabeto[j]:
-      novoCaracter = alfabeto[j - 3]
-      novoNome.append(novoCaracter)
-      
-m = ''.join(novoNome)
-print('Texto descriptografado:', m)
+cripTexto = funcoes.criptogra(texto, 4, opcao)
+print(cripTexto)
